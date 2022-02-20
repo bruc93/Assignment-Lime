cd96952ad93ef7653c30d5ac2522cbf3c3da70d7
bruc93 <alexander.bruce93@hotmail.com>
2022-02-20 08:29:53 +0100

    Add meeting file

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Project;
+using System;
+using System.IO;
+
 Console.WriteLine("Hello, World!");
+
+string filePath = Directory.GetCurrentDirectory() + @"\freebusy-1.0.0.txt";
+
+
+Console.WriteLine(filePath);
+
+List<string> lines = File.ReadAllLines(filePath).ToList();
+
+
+foreach (var line in lines)
+{
+    Console.WriteLine(line);
+}
+
+//Console.ReadLine();
+
+
+
+
+
+
+if (args.Length > 0)
+{
+
+}
+else
+{
+    Console.WriteLine("No arguments");
+}
+
